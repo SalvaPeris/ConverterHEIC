a2e29cc4885648882c02db63a9b9bd2fdbf8db6e
SalvaPeris <sperisgimeno@gmail.com>
2024-10-23 12:45:39 +0200

    Added helpers for files.

---
+++ ConverterHEIC/Helpers/FilesCounter.cs
@@ -0,0 +1,13 @@
+﻿using System.IO;
+
+namespace ConverterHEIC.Helpers
+{
+    internal class FilesCounter
+    {
+        public static string[] Counter(string directoryPath, string searchPattern)
+        {
+            string[] heicFiles = Directory.GetFiles(directoryPath, searchPattern, SearchOption.AllDirectories);
+            return heicFiles;
+        }
+    }
+}
---
+++ ConverterHEIC/Helpers/HeicConverter.cs
@@ -0,0 +1,16 @@
+﻿using ImageMagick;
+
+namespace ConverterHEIC.Helpers
+{
+    public class HeicConverter
+    {
+        public void ConvertHeicToJpg(string inputPath, string outputPath, uint quality = 90)
+        {
+            using (var image = new MagickImage(inputPath))
+            {
+                image.Quality = quality;
+                image.Write(outputPath);
+            }
+        }
+    }
+}
